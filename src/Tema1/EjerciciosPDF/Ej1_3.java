d9e25e0a1f98dd699a8ab05582060700bb784488
tobias <tobiasceniceros@gmail.com>
2025-09-26 20:25:05 +0200

    Actualizacion

---
+++ src/Tema1/EjerciciosPDF/Ej1_3.java
@@ -0,0 +1,28 @@
+package Tema1.EjerciciosPDF;
+
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Ej1_3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        double num1 = 0;
+        double num2 = 0;
+
+        try {
+            num1 = sc.nextInt();
+            num2 = sc.nextInt();
+
+            double resultado = num1 + num2;
+            System.out.print("El resultado es: " + resultado);
+
+        } catch (Exception e) {
+            System.out.println("No es un numero " + "\n" + e.getMessage());
+        }
+
+
+
+    }
+}
---
+++ src/Tema1/EjerciciosPDF/Ej1_3V2.java
@@ -0,0 +1,14 @@
+package Tema1.EjerciciosPDF;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Ej1_3V2 {
+    public static void main(String[] args) {
+        File d = new File("C:\\Users\\tobia\\IdeaProjects\\PSP\\src");
+
+        ProcessBuilder pb = new ProcessBuilder("java","Tema1.EjerciciosPDF.Ej1_3V2");
+
+
+    }
+}
